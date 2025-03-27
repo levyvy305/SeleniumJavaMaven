3a923d0d5bc7a08d479f48a3abb02a1c9b72e2d6
levyvy305 <levyvy305@gmail.com>
2025-03-27 22:43:37 +0700

    commit code

---
+++ src/test/java/com/levyvy/Bai7_WebDriver/DemoAddCookie.java
@@ -0,0 +1,31 @@
+package com.levyvy.Bai7_WebDriver;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DemoAddCookie {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://crm.anhtester.com/admin/clients");
+//        driver.findElement(By.xpath(LocatorsCRM.inputEmail)).clear();
+//        driver.findElement(By.xpath(LocatorsCRM.inputPassword)).clear();
+//        driver.findElement(By.xpath(LocatorsCRM.inputEmail)).sendKeys("admin@example.com");
+//        driver.findElement(By.xpath(LocatorsCRM.inputPassword)).sendKeys("123456");
+//        driver.findElement(By.xpath(LocatorsCRM.buttonLogin)).click();
+
+        //Thread.sleep(1000);
+        Cookie newCookie = new Cookie("sp_session", "994344b20bdd5dc743376a3a1ff41cade55eaa31");
+        driver.manage().addCookie(newCookie);
+        driver.navigate().refresh();
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
